8d59171958b50f5b745f793ba0b4263b5b89e120
Ignacio Pereira <62000495+based-on-what@users.noreply.github.com>
2025-12-30 10:45:36 -0300

    5000 -> 8080

--- main.py
+++ main.py
@@ -543,6 +543,7 @@ def recommend():
         return jsonify({'error': 'internal server error'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     # For production use Gunicorn. dev: python main.py
     app.run(host='0.0.0.0', port=port)
+
